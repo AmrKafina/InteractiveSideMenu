56ba3f3a520ec904957658bee0b40332214a8ec9
AmrKafina <amr.kafina@gmail.com>
2019-02-21 10:06:40 +0300

    migrated to swift 4.2

--- Sources/MenuContainerViewController.swift
+++ Sources/MenuContainerViewController.swift
@@ -148,7 +148,7 @@ extension MenuContainerViewController {
         if let currentContentVC = currentContentViewController {
             if currentContentVC != selectedContentVC {
                 currentContentVC.view.removeFromSuperview()
-                currentContentVC.removeFromParentViewController()
+                currentContentVC.removeFromParent()
 
                 setCurrentView(selectedContentVC)
             }
@@ -166,7 +166,7 @@ fileprivate extension MenuContainerViewController {
      - parameter selectedContentVC: The view controller to be added.
      */
     func setCurrentView(_ selectedContentVC: UIViewController) {
-        addChildViewController(selectedContentVC)
+        addChild(selectedContentVC)
         view.addSubviewWithFullSizeConstraints(view: selectedContentVC.view)
         currentContentViewController = selectedContentVC
     }
